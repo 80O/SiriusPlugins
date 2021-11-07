77bbd5b1b50a19a6e2427dc5873f5cda90100f2a
80O <48831268+80O@users.noreply.github.com>
2021-11-07 12:25:47 +0100

    Add avatar poof plugin.

---
+++ Plugins/AvatarPoof/AvatarPoofDefinition.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Sirius.Api.Plugin;
+
+namespace AvatarPoof
+{
+    public class AvatarPoofDefinition : IPluginDefinition
+    {
+        public Type PluginClass() => typeof(AvatarPoofPlugin);
+
+        public string Name => "Avatar Poof";
+        public string Description => "Shows a cloud when changing looks";
+        public Version Version => new(1, 0, 0);
+    }
+}
---
+++ Plugins/AvatarPoof/AvatarPoofPlugin.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Gaia.Api.Rooms;
+using Gaia.Api.Rooms.Modules;
+using Sirius.Api.Game.Rooms.Engine.Unit;
+using Sirius.Api.Plugin;
+
+namespace AvatarPoof
+{
+    public class AvatarPoofPlugin : IPlugin
+    {
+    }
+
+    public class AvatarModule : IRoomSessionModule
+    {
+        private IRoomSession _session;
+        public Task Initialize(IRoomSession session)
+        {
+            _session = session;
+            session.Room.Npcs.Players.PlayerEnteredRoom += SubscribeToPlayer;
+            return Task.CompletedTask;
+        }
+
+        private void SubscribeToPlayer(object sender, EntityEventArgs e)
+        {
+            if (e.Entity is UserEntity user)
+            {
+                if (_session.Room.ConnectedHabbos.TryGetValue(user.OwnerId, out var info))
+                    info.FigureUpdated += (_, _) => e.Entity.Effect(108, 1);
+            }
+        }
+    }
+}
