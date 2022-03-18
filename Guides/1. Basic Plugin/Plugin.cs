0e871be48bd3575e4b14ab2d5411ebd760eef774
80O <48831268+80O@users.noreply.github.com>
2022-03-18 09:33:24 +0100

    Add Gaia command example.

--- Guides/1. Basic Plugin/Plugin.cs
+++ Guides/1. Basic Plugin/Plugin.cs
@@ -30,12 +30,20 @@ namespace Sirius.Examples.Plugin.BasicPlugin
     /// this is going to be our entry point for the plugin.
     /// You can use constructor dependency injection to add additional servers from the API.
     /// Its important that you use dependency injection as much as possible and avoid newing instances.
+    /// Note that this instance is already created BEFORE the whole emulator is fully launched.
     /// </summary>
     public class BasicPlugin : IPlugin
     {
         public BasicPlugin()
         {
             Console.WriteLine("Hello World!");
+
+            /// Please read:
+            /// Its important to understand how Sirius emulator exactly works.
+            /// All Room game instance logic is handled by a seperate task called Gaia. This is to support scaling and distributing load.
+            /// Mixing services from the Sirius namespaces with Gaia namespaces is highly discouraged,
+            /// While this may work for now, as they're all resolved from the same service provider, in the future these processes are further seperated and it won't work anymore.
+            /// If you have to send callbacks or data between Sirius and Gaia, have a look at the plugins like SuperWireds.
         }
     }
 }
--- Guides/2. Command Plugin/Plugin.cs
+++ Guides/2. Command Plugin/Plugin.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Gaia.Api.Commands;
 using Sirius.Api.Game.Commands;
+using Sirius.Api.Game.Rooms;
+using Sirius.Api.Game.Rooms.Engine.Unit;
 using Sirius.Api.Game.User;
 using Sirius.Api.Plugin;
 
@@ -20,7 +23,8 @@ namespace Sirius.Examples.Plugin.CommandPlugin
     }
 
     /// <summary>
-    /// To create a command on Sirius, you must implement the ICommand interface.
+    /// To create a command on Sirius, you must implement the <see cref="ICommand"/> or <see cref="ITargetCommand"/> interface.
+    /// These interfaces are used by Sirius and NOT by Gaia.
     /// </summary>
     public class PingCommand : ICommand
     {
@@ -29,6 +33,20 @@ namespace Sirius.Examples.Plugin.CommandPlugin
         public Task<CommandResult> Handle(IHabbo habbo, string[] parameters)
         {
             habbo.Alert("Pong!");
+            return Task.FromResult(CommandResult.Ok);
+        }
+    }
+
+    /// <summary>
+    /// Gaia commands can use the <see cref="IRoomCommand"/> or <see cref="IRoomTargetCommand"/> interfaces.
+    /// </summary>
+    public class LookAtMeCommand : IRoomTargetCommand
+    {
+        public string Key => "lookatme";
+        public Task<CommandResult> Handle(Room room, Entity actor, Entity target, string[] parameters)
+        {
+            target.LookAt(actor);
+            actor.Say($"Look at me, {target.OwnerName}! I'm the captain now!");
             return Task.FromResult(CommandResult.Ok);
         }
     }
